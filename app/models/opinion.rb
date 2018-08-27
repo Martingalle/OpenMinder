72f7a5ab67cc4dafea9550d1a3bcdbacde115613
edouard piron <ed.piron@gmail.com>
2018-08-27 18:51:26 +0200

    associations corrections

--- app/models/opinion.rb
+++ app/models/opinion.rb
@@ -1,6 +1,5 @@
 class Opinion < ApplicationRecord
-  belongs_to :user_creator, class_name: 'User'
+  belongs_to :creator, class_name: 'User'
   has_many :tracks
-
   has_many :genres, through: :tracks
 end
