7562e71fea356792941ab41a51bf3eda5aa74b7b
Martin <whoismartin@hotmail.com>
2018-08-27 17:06:13 +0200

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'opinions#index'
+
+  resources :opinions, only: [:index, :show, :create] do
+    resources :tracks, only: [:index, :create]
+    resources :votes, only: [:create, :update]
+  end
+  namespace :user, only: [:show, :update]
+  namespace :admin do
+    resources :opinions, only: [:index, :update, :destroy]
+    resources :tracks, only: [:index, :update, :destroy]
+    resources :genres, only: [:index, :update]
+    resources :users, only: [:index, :show]
+  end
+
 end
