722388c7a6a8c91cbf99b902689cfe4522561cf4
edouard piron <ed.piron@gmail.com>
2018-08-28 19:05:41 +0200

    new genres sort in opinions

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,6 +1,12 @@
 class GenresController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:show]
   def show
+    # ---------------------------------------------------------
+    # USER STORIES
+    # - displaying genre informations
+    # - displaying main opinions related to the genre => instance methods
+    # - displaying secondary opinions related to the genre => instance methods
+    # ---------------------------------------------------------
     @genre = Genre.find(params[:id])
-    @opinions_related = Opinion.where(main_genre: @genre) # add all opinions related to this genre
   end
 end
