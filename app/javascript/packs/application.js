065dc04c69ee667aee7720a078fda75477da5550
Martin <whoismartin@hotmail.com>
2018-08-31 16:41:55 +0200

    mission ok on youtube

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,27 +1,5 @@
 import 'bootstrap';
-import '../openminder/youtube-api.js'
-import '../openminder/category-choice.js'
-import '../openminder/youtube_playlist'
-
-// PLAYER
-
-// on page load je sélectionne le 1er
-const first = document.querySelector(".tracks-js");
-first.classlist.add("opinion-track-selected");
-
-const array = document.querySelectorAll(".tracks-js");
-
-array.forEach((element) => {
-
-  element.addEventListener("click", () => {
-    array.forEach((div) => {
-    if (div.classList.contains("opinion-track-selected")) {
-      div.classList.remove("opinion-track-selected");
-    }
-
-    });
-
-    element.classList.add("opinion-track-selected");
-  });
-
-});
+import '../openminder/youtube-api.js';
+import '../openminder/category-choice.js';
+import '../openminder/youtube_playlist';
+import '../openminder/player.js';
