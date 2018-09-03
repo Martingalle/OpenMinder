3363214b11535a6fe04548319af68602e2fba383
Jennifer Vermorel <jennifer.vermorel@gmail.com>
2018-09-03 16:23:14 +0200

    jquery for login slider

--- app/javascript/openminder/slider.js
+++ app/javascript/openminder/slider.js
@@ -1,18 +1,21 @@
 $(document).ready(function(){
-  $('#goRight').on('click', function(){
+  $('#goRight').on('click', function(e){
+    console.log("hey");
     $('#slideBox').animate({
-      'marginLeft' : '0'
+      'marginLeft' : '0%'
     });
     $('.topLayer').animate({
       'marginLeft' : '100%'
     });
+    e.preventDefault();
   });
-  $('#goLeft').on('click', function(){
+  $('#goLeft').on('click', function(e){
     $('#slideBox').animate({
       'marginLeft' : '50%'
     });
     $('.topLayer').animate({
       'marginLeft': '0'
     });
+    e.preventDefault();
   });
 });
