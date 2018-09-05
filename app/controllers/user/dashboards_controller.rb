c3d3009db7979cce5d13f6936c0beb856d539543
Jennifer Vermorel <jennifer.vermorel@gmail.com>
2018-09-05 18:23:16 +0200

    user dashboard

--- app/controllers/user/dashboards_controller.rb
+++ app/controllers/user/dashboards_controller.rb
@@ -3,8 +3,9 @@ class User::DashboardsController < ApplicationController
     @tracks_added = current_user.tracks_added
     @tracks_voted = current_user.tracks_voted
     @favorite_genre = current_user.genres_appreciations_desc.first[:genre]
+    @second_favorite_genre = current_user.genres_appreciations_desc[1][:genre]
+    @third_favorite_genre = current_user.genres_appreciations_desc[2][:genre]
     authorize [:user, @tracks_added]
     authorize [:user, @tracks_voted]
-    authorize [:user, @favorite_genre]
   end
 end
