7f3c71da18e7655f44cdb49049b9702b1f895ad4
Jennifer Vermorel <jennifer.vermorel@gmail.com>
2018-09-05 13:08:19 +0200

    user dashboard almost finished

--- app/javascript/openminder/display_user_content.js
+++ app/javascript/openminder/display_user_content.js
@@ -1,10 +1,8 @@
 $(document).ready(function() {
   $('#show-tracks').on('click',function(){
-    console.log("tracks");
     $( ".user-track.added.hidden" ).removeClass( "hidden" );
   });
   $('#show-voted-tracks').on('click',function(){
-    console.log("voted");
     $( ".user-track.voted.hidden" ).removeClass( "hidden" );
   });
 });
