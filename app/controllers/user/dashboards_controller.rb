31022b4df1e92f897a16d5d786bf48f1113c4395
edouard piron <ed.piron@gmail.com>
2018-09-05 17:34:24 +0200

    favorite genres

--- app/controllers/user/dashboards_controller.rb
+++ app/controllers/user/dashboards_controller.rb
@@ -2,7 +2,9 @@ class User::DashboardsController < ApplicationController
   def show
     @tracks_added = current_user.tracks_added
     @tracks_voted = current_user.tracks_voted
+    @favorite_genre = current_user.genres_appreciations_desc.first[:genre]
     authorize [:user, @tracks_added]
     authorize [:user, @tracks_voted]
+    authorize [:user, @favorite_genre]
   end
 end
