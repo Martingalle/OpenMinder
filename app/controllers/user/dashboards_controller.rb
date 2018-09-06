4cefbfe15a9bafbc61ba7888449b6e3b35e3f64f
edouard piron <ed.piron@gmail.com>
2018-09-06 16:54:33 +0200

    val seed

--- app/controllers/user/dashboards_controller.rb
+++ app/controllers/user/dashboards_controller.rb
@@ -3,19 +3,19 @@ class User::DashboardsController < ApplicationController
     @tracks_added = current_user.tracks_added
     @tracks_voted = current_user.tracks_voted
 
-    @favorite_genre = current_user.genres_appreciations_desc.first[:genre]
     @favorite_genre_match = false
-    @discover = @favorite_genre.opinions.uniq
-    discover_size = @discover.size
-    @favorite_genre_match = true if discover_size >= 2
-
-    if discover_size >= 3
-      @discover = @favorite_genre.opinions.uniq[0..2]
+    if current_user.genres_appreciations_desc.nil?
+      @discover = Opinion.order('RANDOM()').limit(3)
     else
+      @favorite_genre = current_user.genres_appreciations_desc.first[:genre]
       @discover = @favorite_genre.opinions.uniq
-      @discover << Opinion.order('RANDOM()').limit(3 - discover_size)
+      @favorite_genre_match = true if @discover.size >= 2
+      if @discover.size >= 4
+        @discover = @favorite_genre.opinions.uniq[0..3]
+      else
+        @discover = @favorite_genre.opinions.uniq
+      end
     end
-    @discover = @discover.flatten.uniq
     authorize [:user, @tracks_added]
     authorize [:user, @tracks_voted]
   end
